f3d99dae50138bc78cd394548522d73d753e38c8
Temilola Aderibigbe <temz4rill@gmail.com>
2022-12-10 01:33:38 -0500

    Day 09 Part 1 and Part 2

--- src/main/kotlin/solutions/Day09.kt
+++ src/main/kotlin/solutions/Day09.kt
@@ -1,31 +1,82 @@
 package solutions
 
 import utils.readInput
-import kotlin.collections.ArrayList
+
+data class Point(var x: Int, var y: Int)
 
 class Day09 {
     private var input = readInput("day09/Day09")
 
     fun practice() {
         input = readInput("day09/Practice")
+
         println("Part 1 is ${part01()}")
         println("Part 2 is ${part02()}")
     }
 
     fun part01(): Int {
-        preload()
-        var count = 0
-
-        return count
+        return solver(2)
     }
 
     fun part02(): Int {
-        preload()
-        var highestScenic = 0
-
-        return highestScenic
+        return solver(10)
     }
 
-    private fun preload() {
+    private fun solver(size: Int): Int {
+        val visited = mutableSetOf<Point>()
+        val points = (1..size).map { Point(0, 0) }
+        val head = points.last()
+
+        input.forEach {
+            val (direction, amount) = it.split(" ")
+            repeat(amount.toInt()) {
+                when (direction) {
+                    "L" -> {
+                        head.x = head.x - 1
+                    }
+
+                    "U" -> {
+                        head.y = head.y - 1
+                    }
+
+                    "R" -> {
+                        head.x = head.x + 1
+                    }
+
+                    "D" -> {
+                        head.y = head.y + 1
+                    }
+                }
+
+                points.zipWithNext().reversed().forEach { (l, r) ->
+                    if (!isTouching(l, r)) {
+                        val t = r.direction(l)
+                        l.x += t.x
+                        l.y += t.y
+                    }
+                }
+
+                visited += points.first().copy()
+            }
+        }
+
+        return visited.size
     }
+
+    private fun isTouching(head: Point, tail: Point): Boolean {
+        listOf(-1, 0, 1).forEach { x ->
+            listOf(-1, 0, 1).forEach { y ->
+                if (tail.x + x == head.x && tail.y + y == head.y) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private fun Point.direction(point: Point): Point {
+        return Point(sgn(this.x - point.x), sgn(this.y - point.y))
+    }
+
+    private fun sgn(n: Int) = if (n < 0) -1 else if (n > 0) 1 else 0
 }
